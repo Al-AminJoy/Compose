34940c6f98e33f56ed7e8b0bd74167cd6d27c9fe
Al-AminJoy <alaminislam3555@gmail.com>
2023-06-20 12:36:15 +0600

    Basics

--- app/src/main/java/com/alamin/compose/MainActivity.kt
+++ app/src/main/java/com/alamin/compose/MainActivity.kt
@@ -10,21 +10,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,23 +43,42 @@ import androidx.compose.ui.unit.sp
 import com.alamin.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTheme {
 
-                val count = remember{
-                    mutableStateOf(0)
-                }
+                var name by remember{ mutableStateOf("") }
 
-               Column(modifier = Modifier.fillMaxSize(),
-               verticalArrangement = Arrangement.Center,
-               horizontalAlignment = Alignment.CenterHorizontally) {
-                   Text(text = "${count.value}", fontSize = 20.sp)
-                   Button(onClick = {
-                        count.value++
-                   }) {
-                        Text(text = "Click Me", fontSize = 16.sp)
+                var names by remember{ mutableStateOf(listOf<String>()) }
+
+               Column(modifier = Modifier
+                   .fillMaxSize()
+                   .padding(20.dp)) {
+                   Row(modifier = Modifier.fillMaxWidth()) {
+                       OutlinedTextField(
+                           value = name,
+                           onValueChange = { text -> name = text },
+                          modifier = Modifier.weight(1f))
+                       Spacer(modifier = Modifier.width(16.dp))
+                       Button(onClick = {
+                           if (name.isNotBlank()){
+                               names = names + name
+                               name = ""
+                           }
+                       }) {
+                           Text(text = "Add")
+                       }
+                   }
+                   
+                   LazyColumn(){
+                       items(names){ data ->
+                           Text(text = data, modifier = Modifier
+                               .fillMaxWidth()
+                               .padding(4.dp))
+                           Divider()
+                       }
                    }
                }
             }
